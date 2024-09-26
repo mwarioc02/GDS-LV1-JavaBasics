491dfc4a50165aa757182ca8d27e9ed7720b7a2f
Daniel Zettel <daniel.zettel@lv.campus02.at>
2024-09-26 14:47:15 +0200

    Erste Klasse erstellt

---
+++ src/MyFirstClass.java
@@ -0,0 +1,17 @@
+public class MyFirstClass {
+
+    // Erstellen der main-Methode:
+    // a) main + tab
+    // b) psvm + tab
+    public static void main(String[] args) {
+        // Und hier startet mein Programm
+        System.out.println("Ich werde in der Konsole angezeigt");
+
+        //sout + tab
+        System.out.println("Zeile zwei");
+
+
+        System.out.println("Programm Ende");
+        // Hier endet mein Programm
+    }
+}
