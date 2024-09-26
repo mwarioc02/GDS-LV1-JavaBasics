7a8cb3f4396d0c37823895b6ea1c79e91544ed5e
Daniel Zettel <daniel.zettel@lv.campus02.at>
2024-09-26 16:09:50 +0200

    First Calculator with Comments

---
+++ src/Calculator.java
@@ -0,0 +1,32 @@
+public class Calculator {
+
+
+    /**
+     * java doc kommentar
+     *
+     * @param args programm argumente
+     */
+    public static void main(String[] args) {
+
+        // Ich bin ein Kommentar
+        // Ich bin zweites
+
+        /*
+        Hier können
+        mehrere Zeilen
+        von Kommentaren stehen
+         */
+        String name = "MyCalculator";
+        int a = 3;
+        int b = 2;
+        int result;
+
+        result = a + b;
+
+        System.out.println("Mein Name: " + name);
+        System.out.println("result = " + result);
+
+        // berechnung direkt sout
+        System.out.println(a + b);
+    }
+}
